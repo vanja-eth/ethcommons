dbde12f76435461fa03fc7f3b661243df98456f6
Vanja <vanja.ivancevic@gmail.com>
2024-05-07 00:49:39 +0200

    Fixing mobile menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -8,41 +8,46 @@
   import MenuItems from './MenuItems.svelte'
   import Logo from './Logo.svelte';
 
-  let menuOpen = false;
-
-  function toggleMenu() {
-    menuOpen = !menuOpen;
-  }
-  
   let isDarkMode = browser ? Boolean(document.documentElement.classList.contains('dark')) : true
-  
+  let isMenuOpen = false;
+
   $: currentPage = $page.path;
 
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen; 
+  }
   function disableTransitionsTemporarily() {
     document.documentElement.classList.add('[&_*]:!transition-none')
     window.setTimeout(() => {
       document.documentElement.classList.remove('[&_*]:!transition-none')
     }, 0)
   }
+  
 </script>
 
 <header class="flex items-center justify-between w-full py-4 mx-auto lg:pb-8 gap-16"> 
   <Logo />
 
-  <div class="relative flex justify-around gap-2 items-center">
-    <button class="sm:hidden" on:click={toggleMenu}>
-      <MenuIcon class="h-6 w-6" />
-    </button>
+  <button class="sm:hidden ml-4" on:click={toggleMenu}>
+    <MenuIcon class="h-6 w-6" />
+  </button>
+
+  <div class={`fixed transform top-0 left-0 w-48 h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} sm:hidden ${isDarkMode ? 'bg-black' : 'bg-white'}`}>
+    <ul class="flex flex-col gap-2 text-sm w-full p-4">
+      <MenuItems {navMenu} />
+    </ul>
+  </div>
+
+  <div class="relative flex justify-around gap-2 items-center sm:flex hidden">
     
-    <nav aria-label="Global" class={`${menuOpen ? 'block' : 'hidden'} absolute top-full right-3 w-full sm:relative sm:flex sm:items-center bg-white sm:bg-transparent`}>
       <ul class="flex flex-col sm:flex-row gap-2 text-sm w-full sm:w-auto">
         <MenuItems {navMenu} />
       </ul>
-    </nav>
 
 
     <div class="sm:flex hidden justify-center p-1 relative sm:w-auto sm:-space-x-px overflow-hidden sm:rounded">
     </div>
+    
     <style>
       .rotate {
         transition: transform 0.3s;
@@ -53,29 +58,34 @@
     </style>
     
     <button
-      type="button"
-      role="switch"
-      aria-label="Toggle Dark Mode"
-      aria-checked={isDarkMode}
-      class="w-5 h-5 sm:h-8 sm:w-8 sm:p-1 transition-colors duration-200"
-      on:click={() => {
-        isDarkMode = !isDarkMode
-        localStorage.setItem('isDarkMode', isDarkMode.toString())
-    
-        disableTransitionsTemporarily()
-    
-        if (isDarkMode) {
-          document.querySelector('html').classList.add('dark')
-        } else {
-          document.querySelector('html').classList.remove('dark')
-        }
-      }}
-    >
-      {#if isDarkMode}
-        <MoonIcon class="hidden text-zinc-500 dark:block hover:text-yellow-500 transition-colors duration-200 rotate" />
-      {:else}
-        <SunIcon class="block text-zinc-400 dark:hidden hover:text-orange-500 transition-colors duration-200 rotate" />
-      {/if}
-    </button>
+    type="button"
+    role="switch"
+    aria-label="Toggle Dark Mode"
+    aria-checked={isDarkMode}
+    class="w-5 h-5 sm:h-8 sm:w-8 sm:p-1 transition-colors duration-200"
+    on:click={() => {
+      isDarkMode = !isDarkMode
+      localStorage.setItem('isDarkMode', isDarkMode.toString())
+  
+      disableTransitionsTemporarily()
+  
+      if (isDarkMode) {
+        document.querySelector('html').classList.add('dark')
+      } else {
+        document.querySelector('html').classList.remove('dark')
+      }
+  
+      // Close the menu on mobile
+      if (window.innerWidth <= 768) {
+        isMenuOpen = false;
+      }
+    }}
+  >
+    {#if isDarkMode}
+      <MoonIcon class="hidden text-zinc-500 dark:block hover:text-yellow-500 transition-colors duration-200 rotate" />
+    {:else}
+      <SunIcon class="block text-zinc-400 dark:hidden hover:text-orange-500 transition-colors duration-200 rotate" />
+    {/if}
+  </button>
   </div>
 </header>
